ef2ab62d5eefbddfb16650f40a43d7804a0b6755
roshankukumkar <roshansk2929@gmail.com>
2025-06-05 13:37:31 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Serve frontend files from "public" folder
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('✅ New user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('❌ User disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('🚀 Server running on http://localhost:3000');
+});
